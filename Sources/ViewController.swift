69eb602a7745e970db45b8b31167285f8d15f411
Jake Prickett <jprickett821@gmail.com>
2020-07-31 14:19:52 -0400

    Initial commit/setup

---
+++ Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TranslateText-gRPC-iOS
+//
+//  Created by PRICKETT, JACOB on 7/31/20.
+//  Copyright © 2020 Jacob Prickett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
